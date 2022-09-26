0ca94d5b2cf1a0aeb6b714149adc9f60a2f563b2
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-09-26 19:52:35 +0100

    Update Case2Matthijs.py

--- Case2Matthijs.py
+++ Case2Matthijs.py
@@ -21,6 +21,19 @@ class Land:
 
 url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly"
 
+querystring = {"lat":"52.5","lon":"4.8"}
+
+headers = {
+ 	"X-RapidAPI-Key": "a929aa606bmsh42432cc9b369422p1b8238jsnce8e2abf2f38",
+ 	"X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
+tekst = response.json()
+df = pd.DataFrame.from_dict(tekst)
 
 
 
@@ -41,19 +54,7 @@ original_list = ['Frankrijk, Arras', 'Ecuador, Guayaquil']
 result = st.selectbox('Selecteer het land', original_list)
 st.write(f'De gekozen plek {result}')
 
-querystring = {"lat":"52.5","lon":"4.8"}
 
-headers = {
- 	"X-RapidAPI-Key": "a929aa606bmsh42432cc9b369422p1b8238jsnce8e2abf2f38",
- 	"X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
-}
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
-
-tekst = response.json()
-df = pd.DataFrame.from_dict(tekst)
 
 
 print(df2.info())
