ae3a14ca0df05e5ab2e7ab111acb0e2db5d97c3e
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-09-26 20:38:12 +0100

    Update Case2Matthijs.py

--- Case2Matthijs.py
+++ Case2Matthijs.py
@@ -46,7 +46,7 @@ st.write(f'De gekozen plek {result}')
 for obj in combo_list:
    if (result == obj.land):
       querystring = {"lat": obj.latitude,"lon":obj.longitude}
-print (queryatring)
+print (querys3tring)
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 print(response.text)
