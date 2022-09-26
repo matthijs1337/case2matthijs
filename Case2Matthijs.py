52f20d9f9219d094852cbd348d91d0b045d35530
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-09-26 20:24:50 +0100

    Update Case2Matthijs.py

--- Case2Matthijs.py
+++ Case2Matthijs.py
@@ -32,7 +32,7 @@ headers = {
 combo_list=[]
 original_list = []
 combo_list.append(Land("Frankrijk, Arras", 2.77, 50.29))
-combo_list.append(Land("Ecuador, Guayaquil, -79.897453, -2.203816))
+combo_list.append(Land("Ecuador, Guayaquil", -79.897453, -2.203816))
 combo_list.append(Land("Turkye, Ankara", 32.8597, 39.9334))
 
 for obj in combo_list:
