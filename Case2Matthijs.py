4010d742bdd578419215217a470840296c7c05a2
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-09-28 14:14:02 +0100

    Update Case2Matthijs.py

--- Case2Matthijs.py
+++ Case2Matthijs.py
@@ -30,8 +30,8 @@ url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly"
 
 
 headers = {
-  "X-RapidAPI-Key":"f63be25ec8msh09be29d15fcc4fbp1f0f6ejsn82d64b7f3674",
- 	#"X-RapidAPI-Key": "a929aa606bmsh42432cc9b369422p1b8238jsnce8e2abf2f38",
+ # "X-RapidAPI-Key":"f63be25ec8msh09be29d15fcc4fbp1f0f6ejsn82d64b7f3674",
+ 	"X-RapidAPI-Key": "a929aa606bmsh42432cc9b369422p1b8238jsnce8e2abf2f38",
  	"X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
 }
 
