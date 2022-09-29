445d5bf992affc8b0c7f7b8093f39d456342e650
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-09-29 13:08:44 +0100

    Update Case2Matthijs.py

--- Case2Matthijs.py
+++ Case2Matthijs.py
@@ -17,7 +17,8 @@ from dateutil.relativedelta import relativedelta # to add days or years
 import plotly.io as pio
 pio.renderers.default = 'browser'
 
-
+CURRENT_THEME = "light"
+IS_DARK_THEME = False
 
 
 
